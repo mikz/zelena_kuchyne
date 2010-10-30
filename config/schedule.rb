f6d5d1e7be752095bf9a4c106f7220e9f9198bad
Michal Cichra <michal@vs718.server4u.cz>
2010-10-30 13:18:29 +0200

    added ability to run db backup from cron

---
+++ config/schedule.rb
@@ -0,0 +1,11 @@
+env :DIR, '/var/backup/zelena_kuchyne'
+
+set :job_template, 'bash -l -c ":job"'
+
+every 1.day, :at => "02:00 am" do
+  rake "bootstrap:backup DIR=$DIR MAILTO=backup@zelenakuchyne.cz"
+end
+
+#every 1.day, :at => "02:30 am" do
+# command "bzip2 $DIR/*.sql"
+#end
