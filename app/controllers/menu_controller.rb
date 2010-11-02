5ad38d62deff6a1f83123ed4f6da1caa0310ac85
Michal Cichra <michal.cichra@gmail.com>
2010-11-02 17:25:02 +0100

    removed DEBUG statement

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -78,7 +78,6 @@ class MenuController < ApplicationController
       @scheduled_items[day.scheduled_for] = {:categories => {}, :menus => []}
     end
     dates = @days.collect &:scheduled_for
-    DEBUG {%w{@days dates}}
     
     @categories = MealCategory.find :all, :include => [{:meals => :scheduled_meals}], :conditions => ["scheduled_meals.scheduled_for IN (?) AND scheduled_meals.invisible = false", dates]
     @scheduled_bundles = ScheduledBundle.find :all, :conditions => ["scheduled_bundles.scheduled_for IN (?) AND scheduled_bundles.invisible = false", dates], :include => [{:bundle => {:meal => :meal_category}}]
