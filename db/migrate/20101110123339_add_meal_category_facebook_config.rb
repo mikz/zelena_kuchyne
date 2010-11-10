402bf1492537421e289e49aa45b12b1794fab70f
Michal Cichra <michal.cichra@gmail.com>
2010-11-10 13:41:04 +0100

    added ability to turn off facebook like button for some categories

---
+++ db/migrate/20101110123339_add_meal_category_facebook_config.rb
@@ -0,0 +1,9 @@
+class AddMealCategoryFacebookConfig < ActiveRecord::Migration
+  def self.up
+    add_column :meal_categories, :facebook, :boolean, :null => false, :default => true
+  end
+
+  def self.down
+    remove_column :meal_categories, :facebook
+  end
+end
