f3d70f631857b4aba3d24ea1d9e886f694800590
mikz <m@o2h.cz>
2011-03-19 23:22:30 +0100

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,13 @@ config.action_controller.perform_caching             = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default_url_options = { :host => "zelenakuchyne.cz" }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+   :address              => "localhost",
+   :domain               => "zelenakuchyne.cz"
+}
 
-ActiveRecord::Base.colorize_logging = false
+config.active_record.colorize_logging = false
 config.log_level = :debug
