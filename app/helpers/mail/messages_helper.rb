c8729480a09fdef7e0d40abf5911578bb7ee5984
mikz <m@o2h.cz>
2011-03-19 17:28:00 +0100

    removed products

--- app/helpers/mail/messages_helper.rb
+++
@@ -1,8 +0,0 @@
-module Mail::MessagesHelper
-
-  def format_reply(message)
-    reply = "\n\n\n#{format_date(message.created_at)} #{format_time(message.created_at)} <#{message.from_address}>\n"
-    reply += message.body.split(/\n/).collect {|l| "> #{l}"}.join("\n")
-  
-  end
-end
