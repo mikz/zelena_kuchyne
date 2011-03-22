15d93d97b32e50cbe6266b2f421bf674646c363e
mikz <m@o2h.cz>
2011-03-22 14:25:02 +0100

    changed exception notify address

--- config/initializers/exception_notify.rb
+++ config/initializers/exception_notify.rb
@@ -1,5 +1,5 @@
 if defined? ExceptionNotification
   ExceptionNotification::Notifier.sender_address =%("Zelená kuchyně" <error@zelenakuchyne.cz>)
   ExceptionNotification::Notifier.email_prefix = "[ZelenáKuchyně] [ERROR] "
-  ExceptionNotification::Notifier.exception_recipients = %w(michal.cichra@gmail.com)
+  ExceptionNotification::Notifier.exception_recipients = %w(service@o2h.cz)
 end
