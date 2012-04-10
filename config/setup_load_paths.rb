5990e423038f30938a635e4b4ebb307d8013af10
Michal Cichra <michal@o2h.cz>
2012-04-10 16:19:05 +0200

    fix passenger rvm integration

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,9 +1,6 @@
 # -*- encoding : utf-8 -*-
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
-    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-    rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
@@ -14,4 +11,3 @@ end
 
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
-
