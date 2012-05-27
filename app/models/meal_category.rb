87aef9ef9e9c100b9da05a4f18133c97d8305920
Michal Cichra <michal@o2h.cz>
2012-05-27 14:33:36 +0200

    fix meal category validation

--- app/models/meal_category.rb
+++ app/models/meal_category.rb
@@ -3,8 +3,9 @@ class MealCategory < ActiveRecord::Base
   has_many :meals, :order => "meals.name ASC"
   has_many :bundles, :through => :meals
   has_one  :order, :class_name => "MealCategoryOrder", :foreign_key => "category_id", :dependent => :destroy
-  
+
   after_create :create_order
-  
+
+  validates_uniqueness_of :name
 end
 
