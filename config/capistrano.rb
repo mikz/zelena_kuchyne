ce0b36ec2ad8b737e969a38202a8011fba6910cb
Michal Cichra <michal@o2h.cz>
2013-01-11 13:54:14 +0100

    changed repo

--- config/capistrano.rb
+++ config/capistrano.rb
@@ -2,6 +2,7 @@ set :gateway, 'mikz@de.o2h.cz'
 set :domain, "zelenakuchyne.cz"
 set :application, 'zelena_kuchyne'
 
+set :repository, "git@github.com:mikz/zelena_kuchyne.git"
 set :user, 'hosting'
 
 server 'hosting', :app, :db, :web, :primary => true
