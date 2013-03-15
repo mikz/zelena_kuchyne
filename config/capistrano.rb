236f7e09c38309cc6d53e289b61e224771f1a63d
Michal Cichra <michal@o2h.cz>
2013-03-15 18:00:01 +0100

    record newrelic deploys

--- config/capistrano.rb
+++ config/capistrano.rb
@@ -1,3 +1,5 @@
+require 'new_relic/recipes'
+
 set :gateway, 'mikz@de.o2h.cz'
 set :domain, "zelenakuchyne.cz"
 set :application, 'zelena_kuchyne'
@@ -6,3 +8,5 @@ set :repository, "git@github.com:mikz/zelena_kuchyne.git"
 set :user, 'hosting'
 
 server 'hosting', :app, :db, :web, :primary => true
+
+after "deploy:update", "newrelic:notice_deployment"
